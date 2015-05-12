1b279f314ed41c7070ef75f76c72281d06ce1770
Odgii <odgii.luv@gmail.com>
2015-05-12 02:32:47 +0200

    Done first 3

---
+++ catalyst_project/Model/SimulationTool.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace catalyst_project.Model
+{
+    public class SimulationTool
+    {
+        private int _Id;
+        private string _Tool;
+
+        public SimulationTool(int id, string tool)
+        {
+            Id = id;
+            Tool = tool;
+
+        }
+
+        public int Id
+        {
+            get
+            {
+                return _Id;
+            }
+
+            set
+            {
+                _Id = value;
+            }
+        }
+
+        public string Tool
+        {
+            get
+            {
+                return _Tool;
+            }
+            set
+            {
+                _Tool = value;
+            }
+        }
+
+    }
+}
--- catalyst_project/View/CombineCatalystWindow.xaml.cs
+++ catalyst_project/View/CombineCatalystWindow.xaml.cs
@@ -10,25 +10,24 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace catalyst_project
+namespace catalyst_project.View
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for CombineCatalystWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class CombineCatalystWindow : Window
     {
-        public MainWindow()
+        public CombineCatalystWindow()
         {
             InitializeComponent();
         }
 
-        private void LoginClicked(object sender, RoutedEventArgs e)
+        private void btn_choose_catalyst_Click(object sender, RoutedEventArgs e)
         {
-            catalyst_project.View.MainApplication new_app = new View.MainApplication();
-            new_app.Show();
+            string id = txb_search_value.Text;
+            MainApplication.chosenID = id;
             this.Close();
         }
     }
